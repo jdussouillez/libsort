bccae00f2d462a09c82a10eb6af9d7c8e29838d7
jdussouillez <junior.dussouillez@gmail.com>
2014-02-03 20:07:11 +0100

    Fix C++ compatibility for bubble and bucket sort

--- include/bubblesort.h
+++ include/bubblesort.h
@@ -1,6 +1,14 @@
 #ifndef BUBBLESORT_H
 #define BUBBLESORT_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void bubblesort(void* base, size_t nmemb, size_t size, int(*compare)(const void*, const void*));
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // BUBBLESORT_H
--- include/bucketsort.h
+++ include/bucketsort.h
@@ -1,6 +1,14 @@
 #ifndef BUCKETSORT_H
 #define BUCKETSORT_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void bucketsort(void* base, size_t nmemb, size_t size, int(*compare)(const void*, const void*));
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // BUCKETSORT_H
