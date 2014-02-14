fad9990e0989f0be975f6bdd40a05bf0636a6f90
jdussouillez <junior.dussouillez@gmail.com>
2014-02-14 20:50:13 +0100

    add 'performance' sample. Refactor quicksort

--- src/quicksort.c
+++ src/quicksort.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "../include/sort.h"
 #include "../include/quicksort.h"
@@ -7,26 +9,29 @@
  * Partition the portion of the array between indexes "begin" and "end".
  */
 static int partition(void* base, int begin, int end, size_t size, int pivot, int(*compare)(const void*, const void*)) {
-  int i, j;
+  int i, index = begin;
+  void* pivot_value = malloc(size);
+  memcpy(pivot_value, base + (pivot * size), size);
   swap(base + (pivot * size), base + (end * size), size);
-  j = begin;
   for (i = begin; i < end; i++) {
-    if (compare(base + (i * size), base + (end * size)) <= 0) {
-      swap(base + (i * size), base + (j * size), size);
-      j++;
+    if (compare(base + (i * size), pivot_value) < 0) {
+      swap(base + (i * size), base + (index * size), size);
+      index++;
     }
   }
-  swap(base + (end * size), base + (j * size), size);
-  return j;
+  swap(base + (index * size), base + (end * size), size);
+  free(pivot_value);
+  return index;
 }
 
 /*
  * Sort the array between indexes "begin" and "end" (recursive).
  */
 static void quicksort_rec(void* base, int begin, int end, size_t size, int(*compare)(const void*, const void*)) {
-  int pivot;
+  int pivot, pivotIndex;
   if (begin < end) {
-    pivot = partition(base, begin, end, size, begin, compare); // begin as pivot
+    pivotIndex = (begin + end) / 2;
+    pivot = partition(base, begin, end, size, pivotIndex, compare);
     quicksort_rec(base, begin, pivot - 1, size, compare);
     quicksort_rec(base, pivot + 1, end, size, compare);
   }
